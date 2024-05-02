a66a04379cbbd144cb1c555bdab907c99898197d
Ashish Bansal <ashishbansal@Ashishs-MacBook-Pro.local>
2024-05-02 20:14:07 +0530

    deploy build on github pages

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import "./App.css";
 import { Navigate, Route, Routes } from "react-router-dom";
 import PageLayout from "./layout/PageLayout";
 import Dashboard from "./features/Dashboard/Dashboard";
+
+// due to some issue in github pages have to remove router specific code in this file only
 function App() {
   return (
     <div className="App">
-      <Routes>
+      <Dashboard />
+      {/* <Routes>
         <Route path="/" element={<PageLayout />}>
           <Route index element={<Navigate to="/tasks" replace />} />
           <Route path="/tasks" element={<Dashboard />} />
         </Route>
-      </Routes>
+      </Routes> */}
     </div>
   );
 }
